5f90e996ac7ddf74f96b649b488f2359aff9486c
Tollboothslyr <kazp.entire@gmail.com>
2018-11-21 18:54:03 -0800

    added full piano support
    todo: add variable frequency bands

--- belagio.py
+++ belagio.py
@@ -7,16 +7,21 @@ import numpy as np
 import RPi.GPIO as GPIO
 import pygame
 import time
+import pygame.midi
+import keyboard #testing only
+pygame.midi.init()
+
+for x in range(0, pygame.midi.get_count()):
+    print(pygame.midi.get_device_info(x))
+
+inp = pygame.midi.Input(1)
 
 relaypin = 4
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False) #looking at this gives me anxiety
 GPIO.setup(relaypin,GPIO.OUT)
 
-pygame.init()
-screen = pygame.display.set_mode((1000, 500))
-white = (255, 255, 255)
-black = (0, 0, 0)
+
 screen.fill(white)
 chunk = 2048
 
@@ -27,61 +32,76 @@ RATE = wf.getframerate()
 # use a Blackman window
 window = np.blackman(chunk)
 # open stream
-p = pyaudio.PyAudio()
-stream = p.open(format =
-                p.get_format_from_width(wf.getsampwidth()),
-                channels = wf.getnchannels(),
-                rate = RATE,
-                output = True)
-lst = []
-data = wf.readframes(chunk)
-thefreq = 0 # start
-a=[]
-s = 0.0
-i=0 #start counter (inital value)
-while True:#len(data) == chunk*swidth: #this is supposed to fit the song but ya know thigns dont always work that way
-    #print("hi")
-    i=i+1
-    #gpio on
-    GPIO.output(relaypin, GPIO.HIGH)
-    thefreq1 = [thefreq]
-    stream.write(data)
-    indata = np.array(wave.struct.unpack("%dh"%(len(data)/swidth), data))#*window
-    fftData=abs(np.fft.rfft(indata))**2
-    which = fftData[1:].argmax() + 1
-    if which != len(fftData)-1:
-       #print("hi")
-        y0,y1,y2 = np.log(fftData[which-1:which+2:])
-        x1 = (y2 - y0) * .5 / (2 * y1 - y2 - y0)
-        # find the frequency and output it
-        thefreq = (which+x1)*RATE/chunk
-        if thefreq >= 5500:  # random integer idk just there becauee if a song goes that high its a fat no from me dog
-            thefreq = 463
-        if thefreq:
-
-            print ("The freq is %f Hz." % (thefreq))
-
-    else:
-        thefreq = which*RATE/chunk
-        if thefreq >= 5500: #random integer idk just there becauee if a song goes that high its a fat no from me dog
-            thefreq = 463
-        #print("hi")
-        print ("The freq is %f Hz." % (thefreq))
-    # read some more data
+print('y= wav file, n = Piano')
+if keyboard.is_pressed('y'): #might be an error with indent btw 
+    p = pyaudio.PyAudio()
+    stream = p.open(format =
+                    p.get_format_from_width(wf.getsampwidth()),
+                    channels = wf.getnchannels(),
+                    rate = RATE,
+                    output = True)
+    lst = []
     data = wf.readframes(chunk)
+    thefreq = 0 # start
+    a=[]
+    s = 0.0
+    i=0 #start counter (inital value)
+    while True:#len(data) == chunk*swidth: #this is supposed to fit the song but ya know thigns dont always work that way
+        #print("hi")
+        i=i+1
+        #gpio on
+        GPIO.output(relaypin, GPIO.HIGH)
+        thefreq1 = [thefreq]
+        stream.write(data)
+        indata = np.array(wave.struct.unpack("%dh"%(len(data)/swidth), data))#*window
+        fftData=abs(np.fft.rfft(indata))**2
+        which = fftData[1:].argmax() + 1
+        if which != len(fftData)-1:
+           #print("hi")
+            y0,y1,y2 = np.log(fftData[which-1:which+2:])
+            x1 = (y2 - y0) * .5 / (2 * y1 - y2 - y0)
+            # find the frequency and output it
+            thefreq = (which+x1)*RATE/chunk
+            if thefreq >= 5500:  # random integer idk just there becauee if a song goes that high its a fat no from me dog
+                thefreq = 463
+            if thefreq:
 
-    thefreq1.append(thefreq)
-    if thefreq1[0] +10 > thefreq1[1] and thefreq1[0] -10 < thefreq1[1]:   #for bunching up frequencys to have notes be more indivdivualistic
-        print("yeet")
-        #keep gpio on
-    else:
-        #turn off gpio
-        print("ouff")
-        GPIO.output(relaypin, GPIO.LOW)
+                print ("The freq is %f Hz." % (thefreq))
 
-    if i>3000:
-        break
-    time.sleep(.16666) #set to half of the bpm of the kkb
+        else:
+            thefreq = which*RATE/chunk
+            if thefreq >= 5500: #random integer idk just there becauee if a song goes that high its a fat no from me dog
+                thefreq = 463
+            #print("hi")
+            print ("The freq is %f Hz." % (thefreq))
+        # read some more data
+        data = wf.readframes(chunk)
+
+        thefreq1.append(thefreq)
+        if thefreq1[0] +10 > thefreq1[1] and thefreq1[0] -10 < thefreq1[1]:   #for bunching up frequencys to have notes be more indivdivualistic
+            print("yeet")
+            #keep gpio on
+        else:
+            #turn off gpio
+            print("ouff")
+            GPIO.output(relaypin, GPIO.LOW)
+
+        if i>3000:
+            break
+        time.sleep(.16666) #set to half of the bpm of the kkb
+if keyboard.is_pressed('y'):
+  while True:
+    if inp.poll():
+        IREAD= inp.read(1000)
+        TP = str(IREAD)
+        tl = TP[8:10]
+        tk = int(tl)
+        NoteFreq = 440 * (1.059463 ** ((tk * .93) - 52.5)) #https://www.desmos.com/calculator/yw21ozsz29
+        print(IREAD)
+        print(NoteFreq) #predicted note freq
+    pygame.time.wait(10) #adjust to fit your liking
+else:
+  print('stop the program')
 
 stream.stop_stream()
 stream.close()
